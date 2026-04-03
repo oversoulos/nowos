4ad35876361b35c7bd21ed61152b097fbe0aea8d
oversoulos <sooversoulo@gmail.com>
2026-04-03 10:24:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function startIdle() {
     clearTimeout(S.idleT);
     S.idleT = setTimeout(() => {
         if (!S.nav && !card.classList.contains('flipped')) setZoom('zoomed');
-    }, 300000);
+    }, 300);
 }
 
 function hex4(x, y) {
